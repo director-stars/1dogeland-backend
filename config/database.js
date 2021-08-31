0f538db6a79795a5a8640b96ecb6fe93e3aa618c
StarsIdea <director_9414@outlook.com>
2021-08-31 23:45:33 +0800

    test

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const parse = require('pg-connection-string').parse;
 
 module.exports = ({ env }) => {
-  // if(env('NODE_ENV') === 'production'){
+  if(env('NODE_ENV') === 'production'){
     const config = parse(process.env.DATABASE_URL);
     return {
       defaultConnection: 'default',
@@ -26,20 +26,20 @@ module.exports = ({ env }) => {
         },
       },
     }
-  // }
-  // return {
-  //   defaultConnection: 'default',
-  //   connections: {
-  //     default: {
-  //       connector: 'bookshelf',
-  //       settings: {
-  //         client: 'sqlite',
-  //         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-  //       },
-  //       options: {
-  //         useNullAsDefault: true,
-  //       },
-  //     },
-  //   },
-  // }
+  }
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'sqlite',
+          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        },
+        options: {
+          useNullAsDefault: true,
+        },
+      },
+    },
+  }
 };
