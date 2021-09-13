8500e5e3b49b98b3fc269c384b3185f9327d4068
StarsIdea <director_9414@outlook.com>
2021-09-13 16:10:09 +0800

    test

--- api/crypto-doge/models/crypto-doge.js
+++ api/crypto-doge/models/crypto-doge.js
@@ -9,7 +9,7 @@ module.exports = {
     lifecycles: {
         // Called before an entry is created
         beforeCreate(data) {
-            
+
         },
         // Called after an entry is created
         afterCreate(result) {},
