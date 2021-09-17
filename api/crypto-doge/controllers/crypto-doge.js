b7b22d9761510abcd29c02e3dd45961368f0a409
StarsIdea <director_9414@outlook.com>
2021-09-17 22:37:55 +0800

    added find_dogefindDogesByOwner function

--- api/crypto-doge/controllers/crypto-doge.js
+++ api/crypto-doge/controllers/crypto-doge.js
@@ -6,19 +6,9 @@
  */
 
 module.exports = {
-    // async create(ctx) {
-    //   console.log(ctx.request.body);
-    //   return ctx.request.body;
-
-        // let entity;
-        // if (ctx.is('multipart')) {
-        //   const { data, files } = parseMultipartData(ctx);
-        //   entity = await strapi.services.crypto_doges.create({...data}, { files });
-        // } else {
-        //   entity = await strapi.services.crypto_doges.create({...ctx.request.body, name: ctx.request.body.name, user: user.id});
-        // }
-        // return sanitizeEntity(entity, { model: strapi.models.profile });
-
-        // return 'name';
-    // },
+    async findDogesByOwner(ctx) {
+        const { owner } = ctx.params;
+        const doges = await strapi.services['crypto-doge'].find({owner: owner});
+        return doges;
+    },
 };
