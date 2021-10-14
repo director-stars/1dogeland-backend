d7c9c3803b1491dfbc783a64b526da734569ac54
StarsIdea <director_9414@outlook.com>
2021-10-14 18:13:04 +0800

    test

--- config/functions/cron.js
+++ config/functions/cron.js
@@ -16,14 +16,18 @@ module.exports = {
    * Every 24hours.
    */
   // '0 0,4,8,12,16,20 * * *': async () => {
-  '0 * * * *': async () => {
+  '0/5 * * * *': async () => {
     console.log('cronjob')
     var doges = await strapi.services['crypto-doge'].find();
+
     doges.forEach(async (doge) => {
       // var fn = Math.round(Math.random()*5) + 5;
       await strapi.services['crypto-doge'].update({id: doge.id}, {
         fightNumber: 3
       })
+      console.log(doge.id);
+      res = await strapi.services['crypto-doge'].findOne({id: doge.id});
+      console.log(res);
     });
   }
 };
