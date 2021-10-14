06579eea6ce767d1e6f7f7f15b67a259d143498e
StarsIdea <director_9414@outlook.com>
2021-10-14 12:11:59 +0800

    cronjob test

--- config/functions/cron.js
+++ config/functions/cron.js
@@ -16,10 +16,10 @@ module.exports = {
    * Every 24hours.
    */
   // '0 0,4,8,12,16,20 * * *': async () => {
-  '10 * * * *': async () => {
+  '15 * * * *': async () => {
     console.log('cronjob')
     var doges = await strapi.services['crypto-doge'].find();
-    doges.forEach(doge => {
+    doges.forEach(async (doge) => {
       // var fn = Math.round(Math.random()*5) + 5;
       await strapi.services['crypto-doge'].update({id: doge.id}, {
         fightNumber: 3
